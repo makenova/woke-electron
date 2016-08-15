3aa3e23babe30d5224f9138caa1633275c4f578b
MATTHEW MCHUGHES <mmchughes@MTM-OKAL005.local>
2016-08-15 12:31:21 -0500

    set up react structure and added title component

---
+++ app/Components/title.component.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Title extends React.Component {
+    render(){
+        return (
+        <div className="title">
+            <h1>Woke</h1>
+        </div>
+        )
+
+    }
+}
+
+export default Title

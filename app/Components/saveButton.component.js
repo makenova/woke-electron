1b8c772e17f605e030b306e1a842a5155b242fa9
MATTHEW MCHUGHES <mmchughes@MTM-OKAL005.local>
2016-08-17 17:15:47 -0500

    added material-ui and modded save button

--- app/Components/saveButton.component.js
+++ app/Components/saveButton.component.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
 
 class SaveButton extends React.Component {
     render(){
+        const style = {
+            margin: "auto"
+        };
         return (
         <div className="save-button">
-            <p>Save</p>
+            <RaisedButton label="Save" primary={true} style={style} />
         </div>
         )
 

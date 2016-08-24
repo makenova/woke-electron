29e1f7618109787e874562b3f979726b4e5dcfe4
MATTHEW MCHUGHES <mmchughes@MTM-OKAL005.local>
2016-08-24 17:05:58 -0500

    converted to dark theme

--- app/Components/saveButton.component.js
+++ app/Components/saveButton.component.js
@@ -8,9 +8,10 @@ class SaveButton extends React.Component {
             display: "block",
             width: "20%"
         };
+        
         return (
         <div className="save-button">
-            <RaisedButton label="Save" primary={true} style={style} />
+            <RaisedButton label="Save" primary={true} style={style} backgroundColor="#f3f3f3"/>
         </div>
         )
 
